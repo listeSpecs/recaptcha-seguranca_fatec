16c779b29142b58759783f9c621cb7a5271a6e0b
Caio Liste <73594006+listeSpecs@users.noreply.github.com>
2021-05-21 16:48:42 -0300

    Primeiro commit :)
    Primeiro commit realizado :)

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fetch = require("isomorphic-fetch");
+  
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/submit", (req, res) => {
+  const response_key = req.body["g-recaptcha-response"];
+  const secret_key = "6LeNsOEaAAAAAPUHvcQCNcuhnAFSB0thJo7KPtir";
+
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+
+  fetch(url, {
+    method: "post",
+  })
+    .then((response) => response.json())
+    .then((google_response) => {
+      if (google_response.success == true) {
+        return res.send('<p>Parabéns, você foi validado com sucesso! Humano inteligente. :)</p>');
+      } else {
+        return res.send('<p>Desculpa, infelizmente você não passou na validação. Robô safado. :(</p>');
+      }
+    })
+    .catch(() => {
+      return res.send('<p>Ocorreu um erro, agora nunca saberemos se você é um robô ou não...</p>');
+    });
+});
+  
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
